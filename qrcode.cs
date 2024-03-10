627665c1177e774e263c23d6b2e277dd05e610bd
wenuka lehan <wlghost2022@gmail.com>
2024-03-10 15:14:16 +0530

    wenuka leham

--- qrcode.cs
+++ qrcode.cs
@@ -18,12 +18,18 @@ namespace login
             InitializeComponent();
         }
 
+        public static void abc()
+        {
+            MessageBox.Show("Hello World");
+        }
+
         private void btn_Click(object sender, EventArgs e)
         {
             QRCodeGenerator qr = new QRCodeGenerator();
             QRCodeData data = qr.CreateQrCode(tbx.Text, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(data);
             ptx.Image = qrCode.GetGraphic(5);
+            abc();
         }
     }
 }
